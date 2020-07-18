4c8c0d8aa0e7cffc7bcff60dbdeb53747301c8bb
GabrielCarreiro <63800998+GabrielCarreiro@users.noreply.github.com>
2020-07-17 22:23:09 -0300

    Versão 1.0.0

--- public/scripts/record.js
+++ public/scripts/record.js
@@ -1,28 +1,27 @@
-function Verifica(){
-    pass=document.getElementById("password").value;
-    confirm_pass=document.getElementById("confirm_password").value;
-    
-    if(pass == confirm_pass){ 
+function Verifica() {
+    pass = document.getElementById("password").value;
+    confirm_pass = document.getElementById("confirm_password").value;
+
+    if (pass == confirm_pass) {
         document.getElementById("envia").disabled = false;
-        
-    }else{
+
+    } else {
         document.getElementById("envia").disabled = true;
-        document.getElementById("password").style.color="#f00"; 
-    
-    }    
+        document.getElementById("password").style.color = "#f00";
+
+    }
 }
 
-function validandoSenha(){
+function validandoSenha() {
     password = document.getElementById("password").value;
-    var regex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/; 
+    var regex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
 
 
-if(!regex.exec(password))
-{
-    document.getElementById("password").style.color="#f00";
-}else{
-    document.getElementById("password").style.color="#000";
-    document.getElementById("confirm_password").disabled = false;
-}
+    if (!regex.exec(password)) {
+        document.getElementById("password").style.color = "#f00";
+    } else {
+        document.getElementById("password").style.color = "#000";
+        document.getElementById("confirm_password").disabled = false;
+    }
 
 }
