d3c3ed80b76e76f89d9ead731709f2ae54c46032
GabrielCarreiro <63800998+GabrielCarreiro@users.noreply.github.com>
2020-07-22 11:01:34 -0300

    Initial commit

--- src/server.js
+++ src/server.js
@@ -16,7 +16,7 @@ server.use(express.urlencoded({ extended: true }))
 
 // Configurando o templete engine
 const nunjucks = require("nunjucks")
-nunjucks.configure("src/views",{
+nunjucks.configure("src/views", {
     express: server,
     noCache: true
 })
@@ -24,37 +24,37 @@ nunjucks.configure("src/views",{
 //Configurar Caminhos da minha aplicação
 //Configurando a pagina inical
 server.get("/", (req, res) => {
-     return res.render("index.html")
+    return res.render("index.html")
 
 })
 
 server.post("/?", (req, res) => {
-    
+
     // Atribui na const user, o usuario que foi digitado no login
     const user = req.body.user
     // Atribui na const password, a senha digitada no login
     const password = req.body.password
-    
+
     // Se os campos não tiver valor, retorna para o index
-    if(user == "" || password == ""){
+    if (user == "" || password == "") {
 
         return res.render("index.html")
     }
-    
+
     // Se não, faz um pesquisa no banco e consulta pelos dados
-    db.all(`SELECT user FROM client WHERE user = '${user}' and password = '${password}'`, function(err, rows){
-        if(err) {
+    db.all(`SELECT user FROM client WHERE user = '${user}' and password = '${password}'`, function (err, rows) {
+        if (err) {
             return console.log(err)
         }
         // Atribui na variavel results o rows com valor 1 ou 0
         const results = rows.length
 
         // Compara os valores, se verdadeiro faz o login
-        if(results == 1){
+        if (results == 1) {
             return res.render("search.html")
         }
         // Se não, vai para pagina de cadastro
-            return res.render("record.html")
+        return res.render("record.html")
     })
 })
 
@@ -65,32 +65,32 @@ server.get("/record", (req, res) => {
 
 })
 
-server.post("/saveuser", (req, res) =>{
+server.post("/saveuser", (req, res) => {
 
     const user = req.body.user
     const password = req.body.password
     const confirm_password = req.body.confirm_password
 
-    db.all(`SELECT user FROM client WHERE user = '${user}'`, function(err, rows){
+    db.all(`SELECT user FROM client WHERE user = '${user}'`, function (err, rows) {
 
-        if(err){
+        if (err) {
             console.log(err)
-        } 
-            const results_user = rows.length
+        }
+        const results_user = rows.length
 
 
-        if(results_user >= 1){
-            
-            return res.render("record.html", {results: 1})
-            
-            
+        if (results_user >= 1) {
 
-        }else  if(password != confirm_password){
-                
+            return res.render("record.html", { results: 1 })
+
+
+
+        } else if (password != confirm_password) {
+
             return res.render("record.html")
-            
-            } else {
-                const query = `
+
+        } else {
+            const query = `
                 INSERT INTO client (
                     name,
                     user,
@@ -99,54 +99,59 @@ server.post("/saveuser", (req, res) =>{
                     confirm_password
                 ) values (?,?,?,?,?);
                 `
-                const values = [
-                    req.body.name,
-                    req.body.user,
-                    req.body.email,
-                    req.body.password,
-                    req.body.confirm_password
-        
-                ]
-        
-                function afterInsertData(err) {
-                    if(err) {
-                        return console.log(err)
-                    }
-                    console.log("Cadastrado com sucesso")
-                    console.log(this)
-        
-                    return res.render("index.html")
+            const values = [
+                req.body.name,
+                req.body.user,
+                req.body.email,
+                req.body.password,
+                req.body.confirm_password
+
+            ]
+
+            function afterInsertData(err) {
+                if (err) {
+                    return console.log(err)
                 }
-            db.run(query, values, afterInsertData)
+                console.log("Cadastrado com sucesso")
+                console.log(this)
+
+                return res.render("index.html")
             }
-            
-        })
+            db.run(query, values, afterInsertData)
+        }
 
     })
 
-    
+})
+
+
 
 // Configurando o caminho do create-place
 
 server.get("/create-place", (req, res) => {
 
     return res.render("create-place.html")
-
 })
-                                                                                                                                                                                                           
+
+server.get("/search", (req, res) => {
+
+    return res.render("search.html")
+})
+
+
 server.post("/savepoint", (req, res) => {
 
     const name = req.body.name
 
-   db.all(`SELECT name FROM places WHERE name = '${name}'`, function(err, rows){
-       if(err){
-           console.log(err)
-       }
+    db.all(`SELECT name FROM places WHERE name = '${name}'`, function (err, rows) {
+        if (err) {
+            console.log(err)
+        }
         const results = rows.length
 
-        console.log(results)  
+        console.log(results)
 
-        if(results >= 1){
+        if (results >= 1) {
             return res.render("create-place.html")
         }
         const query = `
@@ -180,11 +185,11 @@ server.post("/savepoint", (req, res) => {
             req.body.close,
             req.body.description,
             req.body.places
-        
+
         ]
 
         function afterInsertData(err) {
-            if(err) {
+            if (err) {
                 return console.log(err)
             }
             console.log("Cadastrado com sucesso")
@@ -192,14 +197,44 @@ server.post("/savepoint", (req, res) => {
 
             return res.render("search.html")
         }
-    db.run(query, values, afterInsertData)
+        db.run(query, values, afterInsertData)
 
 
     })
 
 })
 
-   
+
+// Configurando o caminho do page-results
+
+server.get("/page-results", (req, res) => {
+
+    const search = req.query.search
+
+
+    if (search == "") {
+
+        return res.render("page-results.html", { total: 0 })
+
+    }
+
+    //pegar os dados do banco de dados
+
+    db.all(` SELECT * FROM places WHERE city LIKE '%${search}%'`, function (err, rows) {
+        if (err) {
+            return console.log(err)
+        }
+
+        const total = rows.length
+
+
+        return res.render("page-results.html", { places: rows, total: total })
+    })
+
+
+})
+
+
 
 
 
